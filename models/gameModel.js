dfa93345e5c04b65176a430219a31027ae6ebd65
samuel K <samuelkarsengkhong@gmail.com>
2024-05-01 07:55:27 -0700

    changed model to have default categories and game status to detct win state and difficulties

--- models/gameModel.js
+++ models/gameModel.js
@@ -4,11 +4,11 @@ const gameSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // userID tracks game to the user
     secretCode: { type: [Number], required: true },
     board: { type: [[String]], required: true },
-    feedback: [{ 
-        exactMatches: { type: Number, required: true }, 
-        partialMatches: { type: Number, required: true }, 
-    }],
-    turnCount: { type: Number, required: true }
+    feedback: { type: [String], required: true, default: () => new Array(10).fill('') }, // Array of 10 empty strings
+    status: { type: String, enum: ['ongoing', 'won', 'lost'], default: 'ongoing' }, 
+    difficulty: { type: String, enum: ['easy', 'medium', 'hard'], default: 'easy' }, 
+    turnCount: { type: Number, required: true, default: 1 }
+
 });
 
 
